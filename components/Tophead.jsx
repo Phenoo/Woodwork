cdcaad61bbc01e20c289330b1bf31c4e5e5df07e
Desco4Pf <chidera.paschal@lampnets.com>
2023-01-27 14:41:36 +0100

    first commit

--- components/Tophead.jsx
+++ components/Tophead.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Tophead = ({title, align}) => {
   return (
-    <h4 className={`text-3xl font-bold uppercase ${align}  `}>
+    <h2 className={`text-3xl font-bold uppercase ${align}  `}>
       {title}
-    </h4>
+    </h2>
   )
 }
 
