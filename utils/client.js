154f50811d7c30fce7e04635fe78299b00d4c71e
Phenoo <phenopreneur@outlook.com>
2023-03-23 13:38:46 +0100

    done

--- utils/client.js
+++ utils/client.js
@@ -2,9 +2,9 @@ import createClient from '@sanity/client';
 import createImageUrlBuilder from '@sanity/image-url';
 
 export const client = {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // or the name you chose in step 1
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    projectId: "eeywejc8", // you can find this in sanity.json
+    dataset: "production", // or the name you chose in step 1
+    apiVersion: "2023-02-25",
     useCdn: true,
 }
 
